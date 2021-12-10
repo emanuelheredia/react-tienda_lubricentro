8ab7aeb9817c2121485b7222d9ddf48572e71c9c
emanuel heredia <ema_359@hotmail.com>
2021-12-10 20:25:00 -0300

    Incorporación de contador de productos

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,19 @@
 import React from "react";
+import ItemCount from "./ItemCount";
+
+const onAdd = ()=>{
+    console.log("Agregado a Carrito")
+}
 
 const Item=({producto})=>{
     return (
-        <div className="card" key={producto.id} style={{width:"18rem"}}>
+        <div className="card" >
             <img src={producto.img} className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h4 className="card-title">{producto.marca}</h4>
-                <h5 className="card-title">{producto.tipo}</h5>
-                <p className="card-text">{producto.precio}</p>
-                <a href="..." className="btn btn-primary">Comprar</a>
+                <h4 className="text-center card-title">{producto.marca}</h4>
+                <h5 className="text-center card-title">{producto.tipo}</h5>
+                <h3 className="text-center text-primary card-text">{producto.precio}</h3>
+                <ItemCount stock={producto.stock} inicial={0} onAdd={onAdd}  />
             </div>
         </div>
     )
---
+++ src/components/ItemCount.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {useState}from "react";
+import "../styles.css";
+
+const ItemCount=({stock,inicial,onAdd})=>{
+    let [contador,setContador]=useState(inicial);
+    
+    const sumar=()=>{
+        (contador<stock)?setContador(contador+1):console.log("Sin Stock")
+        }
+    const restar=()=>{
+        (contador>0)?setContador(contador-1):console.log("Sin productos para restar")
+        }
+    
+    if (stock===0 || contador===0){
+    return(
+        <>
+        <div className="d-flex mb-3 justify-content-center">
+        <button className="btn btn-primary" onClick={sumar}>+</button>
+        <h3 className="p-2 border border-primary m-2">{contador}</h3>
+        <button className="btn btn-primary" onClick={restar}>-</button>
+        </div>
+        <div className="d-flex mb-3 justify-content-center">
+        </div>
+        <p className="text-center">Stock Disponible: {stock}</p>
+        </>
+    )}else{
+    return(
+        <>
+        <div className="d-flex mb-3 justify-content-center">
+        <button className="btn btn-primary" onClick={sumar}>+</button>
+        <h3 className="p-2 border border-primary m-2">{contador}</h3>
+        <button className="btn btn-primary" onClick={restar}>-</button>
+        </div>
+        <div className="d-flex mb-3 justify-content-center">
+        <button className="btn btn-success p-3 justify-content-center" onClick={onAdd}>Add to Cart</button>
+        </div>
+        <p className="text-center">Stock Disponible: {stock}</p>
+        </>
+    )}
+}
+export default ItemCount;
